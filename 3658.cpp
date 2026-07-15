2ad732a1fa801b2b84769ca2c8582f4783b5b073
Gaurav Singh Painkara <168909755+Gaurav10223@users.noreply.github.com>
2026-07-16 04:16:39 +0530

    Refactor Solution class with GCD methods

--- 3658.cpp
+++ 3658.cpp
@@ -1,5 +1,5 @@
-class Solution {
 //leetcode potd
+class Solution {
 public:
     int gcd(int x, int y) { return y == 0 ? x : gcd(y, x % y); }
     int gcdOfOddEvenSums(int n) { return gcd(n * n, n * (n + 1)); }
